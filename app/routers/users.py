a0fe96b0143615bd2bea0c91745b742e410a6ab6
BakirovDanil <danil.bakiroff84@gmail.com>
2026-05-27 14:00:28 +0500

    added support refresh-token

--- app/routers/users.py
+++ app/routers/users.py
@@ -6,8 +6,11 @@ from fastapi.security import OAuth2PasswordRequestForm
 from app.models.users import User as UserModel
 from app.schemas import UserCreate, User as UserSchema
 from app.db_depends import get_async_db
-from app.auth import hash_password, verify_password, create_access_token
+from app.auth import hash_password, verify_password, create_access_token, create_refresh_token
 
+import jwt
+from app.config import SECRET_KEY, ALGORITHM
+from app.schemas import UserCreate, User as UserSchema, RefreshTokenRequest
 
 # создание роутера для пользователей
 router = APIRouter(prefix="/users", tags=["users"])
@@ -17,7 +20,8 @@ router = APIRouter(prefix="/users", tags=["users"])
              response_model=UserSchema,
              status_code=status.HTTP_201_CREATED)
 async def create_user(user: UserCreate,
-                      db: AsyncSession = Depends(get_async_db)):
+                      db: AsyncSession = Depends(get_async_db)
+                      ):
     """
     Регистрация нового пользователя с ролью 'buyer' или 'seller'.
     :param user:
@@ -45,9 +49,10 @@ async def create_user(user: UserCreate,
 
 @router.post("/token")
 async def login(form_data: OAuth2PasswordRequestForm = Depends(),
-                db: AsyncSession = Depends(get_async_db)):
+                db: AsyncSession = Depends(get_async_db)
+                ):
     """
-    Аутентификация пользователя и возврат JWT с email, role и id.
+    Аутентификация пользователя и возврат access-JWT и refresh-JWT.
     :param form_data:
     :param db:
     :return:
@@ -63,4 +68,58 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends(),
             headers={"WWW-Authenticate": "Bearer"},
         )
     access_token = create_access_token(data={"sub": user.email, "role": user.role, "id": user.id})
-    return {"access_token": access_token, "token_type": "bearer"}
+    refresh_token = create_refresh_token(data={"sub": user.email, "role": user.role, "id": user.id})
+    return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
+
+
+@router.post("/refresh-token")
+async def refresh_token(body: RefreshTokenRequest,
+                        db: AsyncSession = Depends(get_async_db)
+                        ):
+    """
+    Обновляет refresh-токена, принимая старый refresh-токен в теле запроса
+    :param body:
+    :param db:
+    :return:
+    """
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate refresh token",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    old_refresh_token = body.refresh_token
+
+    try:
+        payload = jwt.decode(old_refresh_token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str | None = payload.get("sub")
+        token_type: str | None = payload.get("token_type")
+
+        # Проверка, что токен действительно refresh
+        if email is None or token_type != "refresh":
+            raise credentials_exception
+
+    except jwt.ExpiredSignatureError:
+        # refresh-токен истек
+        raise credentials_exception
+
+    except jwt.PyJWTError:
+        # подпись неверна или токен поврежден
+        raise credentials_exception
+
+    # проверка, что пользователь существует и активен
+    result = await db.scalars(
+        select(UserModel).where(UserModel.email == email, UserModel.is_active == True)
+    )
+    user = result.first()
+    if user is None:
+        raise credentials_exception
+
+    # генерация нового refresh-токена
+    new_refresh_token = create_refresh_token(
+        data = {"sub": user.email, "role": user.role, "id": user.id}
+    )
+
+    return {
+        "refresh_token": new_refresh_token,
+        "token_type": "beared"
+    }
