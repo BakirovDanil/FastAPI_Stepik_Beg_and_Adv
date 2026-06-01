5131612dc5a4afaa5fc9085464cdfd05f0ee51ad
BakirovDanil <danil.bakiroff84@gmail.com>
2026-06-01 16:38:28 +0500

    added a field 'rating' and link to the table - reviews

--- app/models/products.py
+++ app/models/products.py
@@ -1,6 +1,6 @@
 from decimal import Decimal
 
-from sqlalchemy import String, Boolean, Integer, Numeric, ForeignKey
+from sqlalchemy import String, Boolean, Integer, Numeric, Float, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
@@ -16,6 +16,7 @@ class Product(Base):
     image_url: Mapped[str| None] = mapped_column(String(200), nullable=True)
     stock: Mapped[int] = mapped_column(Integer, nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    rating: Mapped[float] = mapped_column(Float, default=0.0)
 
     # внешний ключ для связи с категориями
     category_id: Mapped[int] = mapped_column(Integer, ForeignKey("categories.id"), nullable=False)
@@ -35,5 +36,5 @@ class Product(Base):
 
     reviews: Mapped[list["Review"]] = relationship(
         "Review",
-        "user"
+        back_populates="product"
     )
